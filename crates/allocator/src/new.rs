6c8e77ebe266ee35e5016c53f0d6147cd9ba9ed4
nan mu <mu.nan.11@outlook.com>
2024-03-03 15:42:00 +0800

    基础分配trait实现

--- crates/allocator/src/new.rs
+++ crates/allocator/src/new.rs
@@ -1,25 +1,32 @@
-use talc::{ErrOnOom, Talc};
+use talc::{ErrOnOom, Span, Talc};
 
 use crate::{AllocError, AllocResult, BaseAllocator, PageAllocator};
 
 struct YourNewAllocator {
     talc: Talc<ErrOnOom>,
+    heap: Span,
 }
 
 impl YourNewAllocator {
     pub const fn new() -> Self {
         Self {
             talc: Talc::new(ErrOnOom),
+            heap: Span::empty(),
         }
     }
 }
 
 impl BaseAllocator for YourNewAllocator {
     fn init(&mut self, start: usize, size: usize) {
-        let memory = talc::Span::new(*start, acme)
-        self.talc.claim(memory);
+        self.heap = unsafe {
+            self.talc
+                .claim(Span::new(start as *mut u8, (start + size) as *mut u8))
+                .unwrap()
+        };
     }
     fn add_memory(&mut self, start: usize, size: usize) -> AllocResult {
+        assert!(!self.heap.is_empty());
+        self.heap.extend(0, size); //感觉有问题，先试试其他
         Ok(())
     }
 }
