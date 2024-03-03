3fa3fbdc500580d630ecc12a9ee2a6366d25a78b
nan mu <mu.nan.11@outlook.com>
2024-03-03 16:00:08 +0800

    部分实现字节分配

--- crates/allocator/src/new.rs
+++ crates/allocator/src/new.rs
@@ -1,8 +1,8 @@
+use crate::{AllocError, AllocResult, BaseAllocator, ByteAllocator};
+use core::{alloc::Layout, ptr::NonNull};
 use talc::{ErrOnOom, Span, Talc};
 
-use crate::{AllocError, AllocResult, BaseAllocator, PageAllocator};
-
-struct YourNewAllocator {
+pub struct YourNewAllocator {
     talc: Talc<ErrOnOom>,
     heap: Span,
 }
@@ -26,7 +26,30 @@ impl BaseAllocator for YourNewAllocator {
     }
     fn add_memory(&mut self, start: usize, size: usize) -> AllocResult {
         assert!(!self.heap.is_empty());
-        self.heap.extend(0, size); //感觉有问题，先试试其他
+        self.heap.extend(0, size); //感觉有问题，先试试
         Ok(())
+    }
+}
+
+impl ByteAllocator for YourNewAllocator {
+    fn alloc(&mut self, layout: Layout) -> AllocResult<NonNull<u8>> {
+        assert!(layout.size() > 0);
+        unsafe { self.talc.malloc(layout).map_err(|_| AllocError::NoMemory) }
+    }
+
+    fn dealloc(&mut self, pos: NonNull<u8>, layout: Layout) {
+        unsafe { self.talc.free(pos, layout) };
+    }
+
+    fn total_bytes(&self) -> usize {
+        self.heap.size()
+    }
+
+    fn used_bytes(&self) -> usize {
+        // self.inner.stats_alloc_actual()
+    }
+
+    fn available_bytes(&self) -> usize {
+        // self.inner.stats_total_bytes() - self.inner.stats_alloc_actual()
     }
 }
